639b14289a896ed5a61ac42da09132deb549b5b4
Saed Yousef <saed.alzaben@gmail.com>
2020-07-04 18:09:08 +0300

    Calculate winner handled, next todo Time travel and storing a history of moves

--- src/index.js
+++ src/index.js
@@ -24,6 +24,9 @@ class Board extends React.Component {
     // Handle the onClick event.
     handleClick(i) {
         const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             squares: squares,
@@ -41,7 +44,14 @@ class Board extends React.Component {
 
     // Rendering the Board.
 	render() {
-		const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        const winner = calculateWinner(this.state.squares);
+        let status;
+		// const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        if (winner) {
+            status = 'Winner ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
 
 		return (
 			<div>
@@ -85,3 +95,24 @@ class Game extends React.Component {
 // ========================================
 
 ReactDOM.render(<Game />, document.getElementById("root"));
+
+// Calculate winner, and determine if there is turn or not.
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
